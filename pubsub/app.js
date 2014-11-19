f0425804d5897071e7f58b89159fcdf770cfde0c
rachelbookhout <rbookhou@gmail.com>
2014-11-19 18:23:34 -0500

    Works on Redis, adding more features

--- pubsub/app.js
+++ pubsub/app.js
@@ -6,8 +6,8 @@ var badges = require('./controllers/badges');
 
 app.use(express.json());
 
-app.post('/', badges.save, badges.send, function(req,res){
-  res.send('\ndone\n\n');
-});
+app.post('/', badges.save, badges.send);
+
+app.get('/badges', badges.get);
 
 app.listen(8000);
--- pubsub/models/badges.js
+++ pubsub/models/badges.js
@@ -1,5 +1,6 @@
 'use strict';
 var redis = require('../lib/redis')
+var broadcast = require('../lib/broadcast')
 /**
 Save bades to database
 @parm{Array} badges
@@ -12,4 +13,22 @@ exports.save = function(badges,callback){
     if (err) return callback(err,null);
     exports.save(badges,callback);
   });
+};
+
+/**
+ Trim the redis list
+*/
+
+exports.trim = function(){
+  redis.ltrim('badges',0,9);
+};
+ /**
+send out badges to the broadcaster
+@param {Array} badges
+@param {Function} callback
+ */
+
+exports.send = function(badges,callback){
+  badges.forEach(broadcast.send);
+  callback(null,null);
 };
